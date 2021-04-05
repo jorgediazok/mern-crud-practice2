a11fe105a74b40f32d50d90f07dd159efc04f2ed
Jorge <jorgediazok@gmail.com>
2021-04-05 16:03:02 -0300

    server created

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
