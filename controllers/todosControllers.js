44a6fee689440976083b37f8f9ffbc2449e464d6
Jorge <jorgediazok@gmail.com>
2021-04-06 02:05:16 -0300

    update done

--- controllers/todosControllers.js
+++ controllers/todosControllers.js
@@ -40,3 +40,52 @@ exports.createTodo = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+exports.deleteTodo = async (req, res) => {
+  try {
+    const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+
+    res.send(deletedTodo);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+exports.updateTodo = async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(200).required(),
+    author: Joi.string().min(3).max(30),
+    uid: Joi.string(),
+    isComplete: Joi.boolean(),
+    date: Joi.date(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const todo = await Todo.findById(req.params.id);
+
+  if (!todo) return res.status(404).send('Todo not found');
+
+  const { name, author, isComplete, date, uid } = req.body;
+
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      req.params.id,
+      {
+        name,
+        author,
+        isComplete,
+        date,
+        uid,
+      },
+      { new: true }
+    );
+    res.send(updatedTodo);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+
+  res.send(updatedTodo);
+};
--- routes/todos.js
+++ routes/todos.js
@@ -5,5 +5,7 @@ const TodoControllers = require('../controllers/todosControllers');
 
 router.get('/', TodoControllers.getTodo);
 router.post('/', TodoControllers.createTodo);
+router.delete('/:id', TodoControllers.deleteTodo);
+router.put('/:id', TodoControllers.updateTodo);
 
 module.exports = router;
