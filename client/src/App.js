8207e1d87d72d3d5b8018ebdbbe75ecc66f19708
Jorge <jorgediazok@gmail.com>
2021-04-06 16:51:55 -0300

    add todo form

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,30 @@ import Navbar from './components/Navbar/Navbar';
 import Todos from './components/Todos/Todos';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import { makeStyles } from '@material-ui/core';
 import { Container } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  contentStyle: {
+    margin: '30px auto',
+  },
+});
+
 function App() {
+  const classes = useStyles();
+
   return (
     <>
       <Router>
         <Container maxWidth="md">
           <Navbar />
-          <Switch>
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/" exact component={Todos} />
-          </Switch>
+          <Container className={classes.contentStyle} maxWidth="sm">
+            <Switch>
+              <Route path="/signin" component={SignIn} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/" exact component={Todos} />
+            </Switch>
+          </Container>
         </Container>
       </Router>
     </>
--- client/src/components/Todos/AddTodos.js
+++ client/src/components/Todos/AddTodos.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import { TextField, Button } from '@material-ui/core';
 import { Send } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  formStyle: {
+    margin: '0px auto',
+    padding: '30px',
+    borderRadius: '9px',
+    boxShadow: '0px 0px 12px -3px #000000',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  submitButton: {
+    marginLeft: '20px',
+  },
+});
 
 const AddTodos = () => {
+  const classes = useStyles();
   return (
     <>
-      <form>
-        <TextField />
-        <Button>
+      <form noValidate autoComplete="off" className={classes.formStyle}>
+        <TextField
+          id="enter-todo"
+          label="Enter ToDo"
+          variant="outlined"
+          autofocus
+          fullWidth
+        />
+        <Button
+          color="primary"
+          variant="contained"
+          type="submit"
+          className={classes.submitButton}>
           <Send />
         </Button>
       </form>
