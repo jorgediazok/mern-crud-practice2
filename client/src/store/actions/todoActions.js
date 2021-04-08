ae08bed6b8ead5498b4295fa2a0356934acacfe2
Jorge <jorgediazok@gmail.com>
2021-04-08 00:19:50 -0300

    added todo actions add todo

---
+++ client/src/store/actions/todoActions.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { url } from '../../api/index';
+
+export const addTodo = (todo) => {
+  return (dispatch, getState) => {
+    axios.post(`${url}/todos`, todo).then((todo) => {
+      dispatch({
+        type: 'ADD_TODO',
+        todo,
+      });
+    });
+  };
+};
